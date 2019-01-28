7f1cf99472daa84573ec6bee8939e57e21b0a0ee
stone ye <stoneye@stones-MacBook-Pro.local>
2019-01-28 21:17:02 +0800

    update

---
+++ leetcode/337.house-robber-iii.java
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode id=337 lang=java
+ *
+ * [337] House Robber III
+ *
+ * https://leetcode.com/problems/house-robber-iii/description/
+ *
+ * algorithms
+ * Medium (46.46%)
+ * Total Accepted:    88.3K
+ * Total Submissions: 188.6K
+ * Testcase Example:  '[3,2,3,null,3,null,1]'
+ *
+ * The thief has found himself a new place for his thievery again. There is
+ * only one entrance to this area, called the "root." Besides the root, each
+ * house has one and only one parent house. After a tour, the smart thief
+ * realized that "all houses in this place forms a binary tree". It will
+ * automatically contact the police if two directly-linked houses were broken
+ * into on the same night.
+ * 
+ * Determine the maximum amount of money the thief can rob tonight without
+ * alerting the police.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [3,2,3,null,3,null,1]
+ * 
+ * ⁠    3
+ * ⁠   / \
+ * ⁠  2   3
+ * ⁠   \   \ 
+ * ⁠    3   1
+ * 
+ * Output: 7 
+ * Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [3,4,5,1,3,null,1]
+ * 
+ * 3
+ * ⁠   / \
+ * ⁠  4   5
+ * ⁠ / \   \ 
+ * ⁠1   3   1
+ * 
+ * Output: 9
+ * Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        
+        return Math.max(search(root,true),search(root,false));
+    }
+    
+    public int search(TreeNode root,boolean choice){
+       if (root==null) return 0;
+        
+        int left = choice?search(root.left,false):Math.max(search(root.left,true),search(root.left,false));
+        int right = choice?search(root.right,false):Math.max(search(root.right,true),search(root.right,false));
+        int sum = left+right+choice?root.val:0;
+        
+        return sum;
+    }
+}
